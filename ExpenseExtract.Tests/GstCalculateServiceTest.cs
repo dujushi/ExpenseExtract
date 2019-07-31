fcc17013bfe44916b7623244452ad403bf3bb96c
dujushi <dujushi@gmail.com>
2019-07-31 13:33:56 +1200

    add unit test for GstCalculateService

---
+++ ExpenseExtract.Tests/GstCalculateServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using ExpenseExtract.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExpenseExtract.Tests
+{
+    [TestClass]
+    public class GstCalculateServiceTest
+    {
+        [TestMethod]
+        public void GetGstExclusiveTotal_ReturnsCorrectValue()
+        {
+            var gstCalculateService = new GstCalculateService();
+            var total = gstCalculateService.GetGstExclusiveTotal(115m);
+            Assert.AreEqual(100, total);
+        }
+    }
+}
