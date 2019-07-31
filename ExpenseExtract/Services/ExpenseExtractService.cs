2afadb4ce2feaf8e5b3399c8c3e576b0820f8201
dujushi <dujushi@gmail.com>
2019-07-31 12:51:28 +1200

    add ExpenseExtractService

---
+++ ExpenseExtract/Services/ExpenseExtractService.cs
@@ -0,0 +1,38 @@
+﻿using ExpenseExtract.ViewModels;
+using System;
+
+namespace ExpenseExtract.Services
+{
+    public class ExpenseExtractService : IExpenseExtractService
+    {
+        public void SetContent(string content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CheckUnclosedTags()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CheckExpenseTag()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CheckTotalTag()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ValidateContent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ExpenseViewModel GetExpense()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
