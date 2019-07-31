d2d368fdefbd7c2a1133505ac1451a8bf260a3fd
dujushi <dujushi@gmail.com>
2019-07-31 13:33:32 +1200

    add GstCalculateService

---
+++ ExpenseExtract/Services/GstCalculateService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExpenseExtract.Services
+{
+    public class GstCalculateService : IGstCalculateService
+    {
+        public decimal GetGstExclusiveTotal(decimal gstInclusiveTotal)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
