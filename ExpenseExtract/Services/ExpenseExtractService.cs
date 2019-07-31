fdd11bcefe234e443ea98ef92dbc9e99f99ef210
William Guo <william.guo@vista.co>
2019-07-31 13:08:46 +1200

    add CheckUnclosedTags implementation

--- ExpenseExtract/Services/ExpenseExtractService.cs
+++ ExpenseExtract/Services/ExpenseExtractService.cs
@@ -1,18 +1,42 @@
-﻿using ExpenseExtract.ViewModels;
+﻿using ExpenseExtract.Exceptions;
+using ExpenseExtract.ViewModels;
 using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace ExpenseExtract.Services
 {
     public class ExpenseExtractService : IExpenseExtractService
     {
+        private string _content;
+
         public void SetContent(string content)
         {
-            throw new NotImplementedException();
+            _content = content;
         }
 
         public void CheckUnclosedTags()
         {
-            throw new NotImplementedException();
+            CheckIsContentInitialised();
+
+            // all open tags
+            const string openTagPattern = @"<([a-zA-Z_]*)>";
+            var openTagRegex = new Regex(openTagPattern);
+            var openTagRegexMatches = openTagRegex.Matches(_content);
+
+            // all close tags
+            const string closeTagPattern = @"</([a-zA-Z_]*)>";
+            var closeTagRegex = new Regex(closeTagPattern);
+            var closeTagRegexMatches = closeTagRegex.Matches(_content);
+
+            // If cannot find matching close tag, unclosed tags exist.
+            var openTags = openTagRegexMatches.Select(match => match.Groups[1].Value).ToList();
+            var closeTags = closeTagRegexMatches.Select(match => match.Groups[1].Value).ToList();
+            var unclosedTags = openTags.Except(closeTags).ToList();
+            if (unclosedTags.Any())
+            {
+                throw new InvalidContentException($"Cannot find matching close tag for {string.Join(", ", unclosedTags)}");
+            }
         }
 
         public void CheckExpenseTag()
@@ -33,6 +57,14 @@ namespace ExpenseExtract.Services
         public ExpenseViewModel GetExpense()
         {
             throw new NotImplementedException();
+        }
+
+        private void CheckIsContentInitialised()
+        {
+            if (_content == null)
+            {
+                throw new InvalidContentException("Content is not set");
+            }
         }
     }
 }
