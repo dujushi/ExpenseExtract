f3a2e06e69d39776f9284da307f783510625d536
dujushi <dujushi@gmail.com>
2019-07-31 13:05:49 +1200

    Add unit tests for CheckUnclosedTags

--- ExpenseExtract.Tests/ExpenseExtractServiceTest.cs
+++ ExpenseExtract.Tests/ExpenseExtractServiceTest.cs
@@ -1,3 +1,5 @@
+using ExpenseExtract.Exceptions;
+using ExpenseExtract.Services;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ExpenseExtract.Tests
@@ -5,5 +7,24 @@ namespace ExpenseExtract.Tests
     [TestClass]
     public class ExpenseExtractServiceTest
     {
+        [DataTestMethod]
+        [DataRow("<expense>")]
+        [DataRow("<expense></ex>")]
+        [DataRow("<expense><total></expense>")]
+        public void CheckUnclosedTags_WhenContentHasUnclosedTags_ThrowsInvalidContentException(string content)
+        {
+            var expenseExtractService = new ExpenseExtractService();
+            expenseExtractService.SetContent(content);
+            Assert.ThrowsException<InvalidContentException>(() => expenseExtractService.CheckUnclosedTags());
+        }
+
+        [TestMethod]
+        public void CheckUnclosedTags_WhenContentDoesNotHaveUnclosedTags_Passes()
+        {
+            var expenseExtractService = new ExpenseExtractService();
+            const string content = "<expense></expense>";
+            expenseExtractService.SetContent(content);
+            expenseExtractService.CheckUnclosedTags();
+        }
     }
 }
---
+++ ExpenseExtract/Exceptions/InvalidContentException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Security.Permissions;
+
+namespace ExpenseExtract.Exceptions
+{
+    [Serializable]
+    public class InvalidContentException : Exception
+    {
+        public InvalidContentException()
+        {
+        }
+
+        public InvalidContentException(string message)
+            : base(message)
+        {
+        }
+
+        public InvalidContentException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+        [SecurityPermission(SecurityAction.Demand, SerializationFormatter = true)]
+        protected InvalidContentException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
