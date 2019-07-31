18c4e469c213a6d6e738116a7e0732de35ac130d
William Guo <william.guo@vista.co>
2019-07-31 13:48:02 +1200

    add GetGstExclusiveTotal implementation

--- ExpenseExtract/Services/GstCalculateService.cs
+++ ExpenseExtract/Services/GstCalculateService.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using Microsoft.Extensions.Options;
+using System;
 
 namespace ExpenseExtract.Services
 {
     public class GstCalculateService : IGstCalculateService
     {
+        private readonly GstCalculateOptions _options;
+
+        public GstCalculateService(IOptions<GstCalculateOptions> options)
+        {
+            _options = options.Value;
+        }
+
         public decimal GetGstExclusiveTotal(decimal gstInclusiveTotal)
         {
-            throw new NotImplementedException();
+            var total = gstInclusiveTotal / (1 + _options.Rate);
+            var rounded = Math.Round(total, 2);
+            return rounded;
         }
     }
 }
