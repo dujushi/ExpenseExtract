5bb89264edf36eecc0017eb9df2c4fd4d1fc736a
dujushi <dujushi@gmail.com>
2019-07-31 12:50:44 +1200

    add IExpenseExtractService

---
+++ ExpenseExtract/Services/IExpenseExtractService.cs
@@ -0,0 +1,14 @@
+﻿using ExpenseExtract.ViewModels;
+
+namespace ExpenseExtract.Services
+{
+    public interface IExpenseExtractService
+    {
+        void SetContent(string content);
+        void CheckUnclosedTags();
+        void CheckExpenseTag();
+        void CheckTotalTag();
+        void ValidateContent();
+        ExpenseViewModel GetExpense();
+    }
+}
